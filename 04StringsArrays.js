84287a6c7eafede21437e8acaef8d971b92d1163
MuditSingh <133003333+BahuBhaiya04@users.noreply.github.com>
2025-08-10 05:42:36 -0700

    Add files via upload

---
+++ 04StringsArrays.js
@@ -0,0 +1,20 @@
+/******************Strings**********************/
+var a= 'StrInG'
+
+// Indexing
+for(let i=0; i<=5; i++){
+    
+    let idx = a[i]; //Indexing
+
+    console.log(`This index is ${i} and letter is ${idx}`);
+}
+
+let len= a.length
+console.log(`length is ${len}`);
+
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+
+console.log(a.slice(1,4));
+
+
