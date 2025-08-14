0c72903ecf4f35c363fca538639b6765724910af
MuditSingh <133003333+BahuBhaiya04@users.noreply.github.com>
2025-08-14 10:08:32 -0700

    Update 08SillyProject.js

--- 08SillyProject.js
+++ 08SillyProject.js
@@ -1,15 +1,17 @@
-// Div Box is targeted
-target = document.body.childNodes[1].children;//HTML collection
-target_length = target.length;//9
+// Div Boxes are Target
+// childNodes[1] Targets container
+target = document.body.childNodes[1].children;     //.children creates array ==>HTML collection<==
+target_length = target.length;                     //==> 9 <==
 
 for(let i=0; i<target_length; i++){
     const r = Math.floor(255*Math.random());
     const g = Math.floor(255*Math.random());
     const b = Math.floor(255*Math.random());
 
-    let color = `rgb(${r},${g},${b})`;
-    console.log(color);
+    let color = `rgb(${r},${g},${b})`;            // Color string Generation as sample ==> rgb(145,253,21) <==
+    console.log(color);                           // Can be commented for checking Values nature 
 
     target[i].style.backgroundColor = color;
 }
+
 
