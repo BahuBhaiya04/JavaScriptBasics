6f317c48bec86cfa55e60f1e88875ec913748fc8
MuditSingh <133003333+BahuBhaiya04@users.noreply.github.com>
2025-08-16 08:43:51 -0700

    Update 09IdClassTargets.js

--- 09IdClassTargets.js
+++ 09IdClassTargets.js
@@ -3,29 +3,29 @@
 
 //Target Element by Class
 
-const target2 = document.getElementsByClassName('box');//Gives all classes as a HTML Collection
-console.log(target2); //Confirm check
+const target2 = document.getElementsByClassName('box');        //Gives all classes as a HTML Collection
+console.log(target2);                                          //Confirm check
 
-target2[2].style.backgroundColor = 'salmon';//indexing to target
+target2[2].style.backgroundColor = 'salmon';                   //indexing to target
 
 //Target by Element Id
 
-const target = document.getElementById('specific-target');//single target specific element
-console.log(target); //confirm check
+const target = document.getElementById('specific-target');     //single target specific element
+console.log(target);                                           //confirm check
 
 target.style.backgroundColor = 'turquoise';
 
 
 //target by querySelector
 const Query_target = document.querySelector('.box');
-console.log(Query_target);//Returns first element that matches class
+console.log(Query_target);                                     //Returns first element that matches class
 
 //To color element
 Query_target.style.backgroundColor = 'rgb(255, 244, 95)';
 
 //QueryselectorAll
 const QueryAll_target = document.querySelectorAll('.box');
-console.log(QueryAll_target);//Returns all classes box as nodelist 
+console.log(QueryAll_target);                                  //Returns all classes box as nodelist 
 
 // To color all elements 
 // use this
@@ -38,11 +38,11 @@ QueryAll_target.forEach(e=>{
     e.style.backgroundColor = 'purple';
 })
 
-document.body.style.backgroundColor = 'beige'; //Just for eyecomfort nothing else
+document.body.style.backgroundColor = 'beige';                //Just for eyecomfort nothing else
 
 
 //Get Element by tagname
-console.log(document.getElementsByTagName('div')); //Returns HTML Collection
+console.log(document.getElementsByTagName('div'));            //Returns HTML Collection
 
 console.log(document.getElementsByName(''));
 
@@ -52,10 +52,11 @@ console.log(document.getElementsByName(''));
 e = document.getElementsByTagName('div');
 
 //elem.matches(css)
-console.log(e[5].matches('#specific-target')); // Returns boolean 
+console.log(e[5].matches('#specific-target'));               // Returns boolean 
 
 //elem.closest(css)
 console.log(e[5].closest('.container'));
 
 //elem1.contains(element2)
 console.log(document.body.getElementsByClassName('container')[0].contains(e[5]));
+
